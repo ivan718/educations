2d8f00303295b58c3642ab7e4d8ac1f1cbc8bc51
ivan718 <vanes200105@gmail.com>
2022-03-16 21:45:02 +0600

    Глава 2

---
+++ 3/dlina_diag.py
@@ -0,0 +1,6 @@
+import math
+
+# Длина диагонали
+l = 4
+w = 10
+d = math.sqrt(1**2 + w**2)
---
+++ 3/№6.py
@@ -0,0 +1,14 @@
+age = 21;
+
+if age <= 1:
+    print("Новорождённый")
+elif 1 < age and age <= 10:
+    print("Ребёнок")
+elif 10 < age and age <= 16:
+    print("Подросток")
+elif 16 < age and age <= 29:
+    print("Типо молодой")
+elif 29 < age and age <= 59:
+    print("Уже взрослый получается")
+else:
+    print("Старый уже стал")
